a4e0670fd3fb6499dc7d50489b57b5f489e51f2d
Evan1208 <44937054+Evan1208@users.noreply.github.com>
2022-01-22 15:08:15 +0800

    文字內容

---
+++ app/src/main/java/com/example/textviewexample/MainActivity.kt
@@ -0,0 +1,245 @@
+package com.example.textviewexample
+
+import android.content.Context
+import android.graphics.*
+import android.graphics.drawable.Animatable
+import android.graphics.drawable.Drawable
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.annotation.RequiresApi
+import androidx.databinding.DataBindingUtil
+import com.example.textviewexample.databinding.ActivityMainBinding
+import android.text.Spanned
+
+import android.text.SpannableString
+import android.text.TextPaint
+import android.text.style.*
+import android.animation.ValueAnimator
+
+import android.animation.FloatEvaluator
+
+import android.animation.ObjectAnimator
+import android.animation.ObjectAnimator.ofFloat
+import android.content.Intent
+import android.text.format.DateUtils
+import android.util.Property
+
+import android.view.animation.LinearInterpolator
+
+
+
+
+
+class MainActivity : AppCompatActivity() {
+
+    private val mBinding: ActivityMainBinding by lazy {
+        DataBindingUtil.setContentView(this, R.layout.activity_main)
+    }
+
+
+    @RequiresApi(Build.VERSION_CODES.S)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setAnimation(mBinding.mOne.compoundDrawables)
+        setAnimation(mBinding.mTwo.compoundDrawables)
+        setAnimation(mBinding.mThree.compoundDrawables)
+        setAnimation(mBinding.mFour.compoundDrawables)
+        setAnimation(mBinding.mSix.compoundDrawables)
+        setAnimation(mBinding.mSeven.compoundDrawables)
+        setAnimation(mBinding.mEight.compoundDrawables)
+
+
+
+        var iShader: Shader = LinearGradient(
+            10f, 10f, 40f, mBinding.mTwelve.textSize,
+            Color.RED, Color.BLUE,
+            Shader.TileMode.CLAMP
+        )
+        mBinding.mTwelve.paint.shader = iShader
+
+        val bitmap = BitmapFactory.decodeResource(
+            resources,
+            R.drawable.pix
+        )
+
+        iShader = BitmapShader(
+            bitmap,
+            Shader.TileMode.REPEAT,
+            Shader.TileMode.REPEAT
+        )
+
+        mBinding.mThirteen.paint.shader = iShader
+
+
+        var iString = SpannableString("Text with underline span")
+        iString.setSpan(UnderlineSpan(), 10, 19, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        mBinding.mFourteen.text = iString
+
+        iString = SpannableString("Text with StrikethroughSpan span")
+        iString.setSpan(StrikethroughSpan(), 10, 19, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        mBinding.mFifteen.text = iString
+
+        iString = SpannableString("Text with SubscriptSpan span")
+        iString.setSpan(SubscriptSpan(), 10, 19, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        mBinding.mSixteen.text = iString
+
+        iString = SpannableString("Text with SuperscriptSpan span")
+        iString.setSpan(SuperscriptSpan(), 10, 19, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        mBinding.mEighteen.text = iString
+
+        iString = SpannableString("Text with BackgroundColorSpan span")
+        iString.setSpan(BackgroundColorSpan(Color.GREEN), 10, 19, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        mBinding.mNineteen.text = iString
+
+        iString = SpannableString("Text with ForegroundColorSpan span")
+        iString.setSpan(ForegroundColorSpan(Color.RED), 10, 19, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        mBinding.mTwenty.text = iString
+
+
+        val iSubstring = "RainBow".lowercase()
+        val iText = "It is RainBow Span!"
+        var iSpannableString = SpannableString(iText)
+        val iStart: Int = iText.lowercase().indexOf(iSubstring)
+        val iEnd: Int = iStart + iSubstring.length
+        iSpannableString.setSpan(
+            RainbowSpan(this), iStart, iEnd, 0
+        )
+
+        mBinding.mTwentyOne.text = iSpannableString
+
+
+        val iAnimatedColorSpan = AnimatedColorSpan(this)
+        iSpannableString = SpannableString(iText)
+        iSpannableString.setSpan(
+            iAnimatedColorSpan, iStart, iEnd, 0
+        )
+
+        val objectAnimator: ObjectAnimator = ofFloat(
+            iAnimatedColorSpan,
+            iAnimatedColorSpan.ANIMATED_COLOR_SPAN_FLOAT_PROPERTY, 0f, 100f
+        )
+        objectAnimator.setEvaluator(
+            FloatEvaluator()
+        )
+        objectAnimator.addUpdateListener { mBinding.mTwentyTwo.text = iSpannableString }
+
+        objectAnimator.interpolator = LinearInterpolator()
+        objectAnimator.duration = DateUtils.MINUTE_IN_MILLIS * 3
+        objectAnimator.repeatCount = ValueAnimator.INFINITE
+        objectAnimator.start()
+
+
+
+        mBinding.mNextPage.setOnClickListener {
+            val iTent = Intent(this, PageOneActivity::class.java)
+            startActivity(iTent)
+        }
+    }
+
+
+
+    private fun setAnimation(pArrayDrawable: Array<Drawable>) {
+        Log.v("aaa","ArrayDrawable size=${pArrayDrawable.size}")
+        for( iDrawable in pArrayDrawable) {
+            if( iDrawable is Animatable) {
+                iDrawable.start()
+            }
+        }
+    }
+}
+
+@RequiresApi(Build.VERSION_CODES.M)
+class AnimatedColorSpan(pContext: Context): CharacterStyle(){
+    private val mArray = IntArray(7)
+    private var mShader: Shader? = null
+    private val mMatrix = Matrix()
+    private var mTranslateXPercentage = 0f
+
+    init {
+        mArray[0] = pContext.getColor(R.color.cff0000)
+        mArray[1] = pContext.getColor(R.color.cFF7f00)
+        mArray[2] = pContext.getColor(R.color.cFFff00)
+        mArray[3] = pContext.getColor(R.color.c00ff00)
+        mArray[4] = pContext.getColor(R.color.c0000ff)
+        mArray[5] = pContext.getColor(R.color.c4b0082)
+        mArray[6] = pContext.getColor(R.color.c9400d3)
+    }
+
+    fun setTranslateXPercentage(percentage: Float) {
+        mTranslateXPercentage = percentage
+    }
+
+    fun getTranslateXPercentage(): Float {
+        return mTranslateXPercentage
+    }
+
+    override fun updateDrawState(tp: TextPaint?) {
+        if (tp != null) {
+            tp.style = Paint.Style.FILL
+            val width: Float = tp.textSize * mArray.size
+            if (mShader == null) {
+                mShader = LinearGradient(0f, 0f, 0f, width, mArray, null, Shader.TileMode.MIRROR)
+            }
+            if( mShader != null) {
+                mMatrix.reset()
+                mMatrix.setRotate(90f)
+                mMatrix.postTranslate(width * mTranslateXPercentage, 0f)
+                mShader!!.setLocalMatrix(mMatrix)
+                tp.shader = mShader
+            }
+        }
+    }
+
+    val ANIMATED_COLOR_SPAN_FLOAT_PROPERTY = object : Property<AnimatedColorSpan, Float>(Float::class.java,
+        "ANIMATED_COLOR_SPAN_FLOAT_PROPERTY") {
+
+        override fun set(span: AnimatedColorSpan?, value: Float?) {
+            if( span != null && value != null) {
+                span.setTranslateXPercentage(value)
+            }
+        }
+
+        override fun get(span: AnimatedColorSpan): Float {
+            return span.getTranslateXPercentage()
+        }
+
+    }
+
+}
+
+
+@RequiresApi(Build.VERSION_CODES.M)
+class RainbowSpan(pContext: Context): CharacterStyle(), UpdateAppearance {
+    private val mArray = IntArray(7)
+
+    init {
+        mArray[0] = pContext.getColor(R.color.cff0000)
+        mArray[1] = pContext.getColor(R.color.cFF7f00)
+        mArray[2] = pContext.getColor(R.color.cFFff00)
+        mArray[3] = pContext.getColor(R.color.c00ff00)
+        mArray[4] = pContext.getColor(R.color.c0000ff)
+        mArray[5] = pContext.getColor(R.color.c4b0082)
+        mArray[6] = pContext.getColor(R.color.c9400d3)
+    }
+
+    override fun updateDrawState(tp: TextPaint?) {
+        if (tp != null) {
+            tp.style = Paint.Style.FILL
+            val shader: Shader = LinearGradient(
+                0f, 0f, 0f,
+                tp.textSize * mArray.size,
+                mArray, null,
+                Shader.TileMode.MIRROR
+            )
+            val matrix = Matrix()
+            matrix.setRotate(90f)
+            shader.setLocalMatrix(matrix)
+            tp.shader = shader
+        }
+
+    }
+
+}
---
+++ app/src/main/java/com/example/textviewexample/PageOneActivity.kt
@@ -0,0 +1,191 @@
+package com.example.textviewexample
+
+import android.content.Context
+import android.graphics.*
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.annotation.RequiresApi
+import androidx.databinding.DataBindingUtil
+
+import android.text.style.*
+
+import android.annotation.SuppressLint
+import android.content.res.Resources
+import android.text.*
+import android.util.AttributeSet
+
+import android.widget.EditText
+import com.example.textviewexample.databinding.ActivityPageOneBinding
+import android.text.style.LeadingMarginSpan
+
+
+
+val Int.dp: Int
+    get() = (this * Resources.getSystem().displayMetrics.density + 0.5f).toInt()
+
+class PageOneActivity : AppCompatActivity() {
+
+    private val mBinding: ActivityPageOneBinding by lazy {
+        DataBindingUtil.setContentView(this, R.layout.activity_page_one)
+    }
+
+
+
+    @RequiresApi(Build.VERSION_CODES.S)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        var iString = SpannableString("Text with underline span")
+        iString.setSpan(Square(this), 10, 19, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+        mBinding.mTwo.text = iString
+
+        //測試過若只有一個點點  無法做累加動作
+//        var iAllText:CharSequence = ""
+//        val iArraySpn = arrayListOf<SpannableString>()
+//        val iSize = 10
+//        var iIndex = 0
+//        do {
+//            iString = SpannableString("Text with underline span $iIndex \naaddkdfk")
+//            iString.setSpan(BulletSpan(15, Color.RED), 0, 0, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+//            iAllText = TextUtils.concat(iAllText, iString)
+////            iArraySpn.add(iString)
+//            iIndex++
+//        }while (iSize > iIndex)
+
+        iString = SpannableString("Text with underline span  \naaddkdfk")
+        iString.setSpan(BulletSpan(15, Color.RED), 0, 0, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        mBinding.mThree.text = iString
+
+
+        val bullets = arrayOf("1.", "2.", "3.", "4.")
+        val itemContents = arrayOf(
+            "那一天，閉目在經殿香霧中，驀然聽見，你誦經中的真言；",
+            "那一月，我搖動所有的經筒，不為超度，只為觸摸你的指尖；",
+            "那一年，磕長頭匍匐在山路，不為覲見，只為貼著你的溫暖；",
+            "那一世，轉山轉水轉佛塔呀，不為修來生，只為途中與你相見。"
+        )
+
+        var iAllText: CharSequence? = ""
+        for (i in bullets.indices) {
+            val aBullet = bullets[i]
+            val t = itemContents[i].trim()
+
+            // 註意此處的換行, 如果沒有換行符, 則系統當做只有一個項目處理
+            val spannableString = SpannableString(t+"\n")
+            spannableString.setSpan(object : LeadingMarginSpan {
+                override fun getLeadingMargin(first: Boolean): Int {
+
+                    // 項目符號和正文的縮進距離, 單位 px
+                    // 我們可以根據 first 來改變第1行和其余行的縮進距離
+                    return 20.dp
+                }
+
+                override fun drawLeadingMargin(
+                    c: Canvas,
+                    p: Paint,
+                    x: Int,
+                    dir: Int,
+                    top: Int,
+                    baseline: Int,
+                    bottom: Int,
+                    text: CharSequence?,
+                    start: Int,
+                    end: Int,
+                    first: Boolean,
+                    layout: Layout?
+                ) {
+
+                    // 只對第1行文本添加項添加符號
+                    if (first) {
+                        val orgStyle = p.style
+                        p.style = Paint.Style.FILL
+                        c.drawText(aBullet, 0f, bottom - p.descent(), p)
+                        p.style = orgStyle
+                    }
+                }
+            }, 0, t.length, 0)
+            iAllText = TextUtils.concat(iAllText, spannableString)
+        }
+
+        mBinding.mFour.text = iAllText
+
+    }
+
+}
+
+@SuppressLint("AppCompatCustomView")
+class LinedEditText: EditText {
+
+
+    constructor(context: Context):super(context)
+
+    constructor(context: Context, attributeSet: AttributeSet): super(context, attributeSet)
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    private fun init() {
+//        paint.style = Paint.Style.STROKE
+//        paint.color = resources.getColor(
+//            R.color.black, null
+//        )
+        paint.strokeWidth = (lineHeight / 10).toFloat()
+        paint.strokeCap = Paint.Cap.ROUND
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    override fun onDraw(canvas: Canvas?) {
+        if( canvas != null) {
+            init()
+            val startX = paddingLeft.toFloat()
+            val stopX = (width - paddingRight).toFloat()
+            val offsetY = (paddingTop
+                    - paint.fontMetrics.top
+                    + paint.strokeWidth / 2)
+            for (i in 0 until lineCount) {
+                val y = offsetY + lineHeight * i
+                canvas.drawLine(
+                    startX, y, stopX, y, paint
+                )
+            }
+        }
+
+        super.onDraw(canvas)
+    }
+
+}
+
+class Square(private val mContext: Context): ReplacementSpan() {
+
+    private var mWidth = 0
+
+    override fun getSize(
+        paint: Paint,
+        text: CharSequence?,
+        start: Int,
+        end: Int,
+        fm: Paint.FontMetricsInt?
+    ): Int {
+        mWidth = paint.measureText(text, start, end).toInt()
+        return mWidth
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    override fun draw(
+        canvas: Canvas,
+        text: CharSequence?,
+        start: Int,
+        end: Int,
+        x: Float,
+        top: Int,
+        y: Int,
+        bottom: Int,
+        paint: Paint
+    ) {
+        paint.color = mContext.resources.getColor(
+            R.color.c0000ff, null
+        )
+        paint.style = Paint.Style.STROKE
+        canvas.drawRect(x, top.toFloat(), x + mWidth, bottom.toFloat(), paint)
+    }
+
+}
